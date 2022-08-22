6f09743eb7df41c937983cf6de8118458b438340
Amulya <amulya.tanksale@gmail.com>
2022-08-22 21:31:03 +0530

    Update RubyException.rb

--- RubyException.rb
+++ RubyException.rb
@@ -1,3 +1,141 @@
+require 'English'
+
+puts $!.inspect
+#Output
+=> nil
+
+begin
+  raise "Oops"
+rescue
+  puts $!.inspect
+  puts $ERROR_INFO.inspect
+end
+#Output
+=> #<RuntimeError: Oops>
+=> #<RuntimeError: Oops>
+
+puts $!.inspect
+#Output
+=> nil
+
+##########
+
+begin
+  raise 'This exception will be rescued!'
+rescue StandardError => e
+  puts "Rescued: #{e.inspect}"
+end
+
+begin
+  raise 'This exception will be rescued!'
+rescue StandardError, AnotherError => e
+  puts "Rescued: #{e.inspect}"
+end
+
+begin
+  raise 'This exception will be rescued!'
+rescue StandardError => e
+  puts "Rescued: #{e.inspect}"
+rescue AnotherError => e
+  puts "Rescued, but with a different block: #{e.inspect}"
+end
+
+################
+
+require "net/http"
+ 
+http = nil
+uri = URI("http://google.com:31/")
+retries = 0
+ 
+begin
+  unless http
+    puts "Opening TCP connection..."
+    http = Net::HTTP.start(uri.host, uri.port, read_timeout: 10)
+  end
+  puts "Executing HTTP request..."
+  puts http.request_get(uri.path).body
+rescue Errno::ECONNREFUSED, Net::ReadTimeout => e
+  if (retries += 1) <= 3
+    puts "Timeout (#{e}), retrying in #{retries} second(s)..."
+    sleep(retries)
+    retry
+  else
+    raise
+  end
+ensure
+  if http
+    puts 'Closing the TCP connection...'
+    http.finish
+  end
+end
+
+
+
+##############
+
+begin
+  date_form = Date.strptime(date, "%m/%d/%Y")
+rescue StandardError
+  # Parse for other formats of date
+  date_form = date.to_date
+end
+super(date_form)
+
+
+###############
+
+
+begin
+  raise 'This exception will be rescued!'
+rescue StandardError => e
+  $stderr.puts e.message
+end
+
+
+#####################
+
+require 'logger'
+
+log = Logger.new(STDERR)
+begin
+  raise
+rescue
+  log.warn('file_name') { "line no." }
+  exit false # Exits with "failure" code
+end
+
+#Output
+=> W, [2022-08-17T17:45:29.515381 #42262]  WARN -- file_name: line no.
+
+############
+
+def destroy
+  user = User.find_by(id: params[:id])
+  return render json: { user: user, status: 200, message: "User deleted successfully" } if user.destroy
+
+  render json: { user: user, status: 500, message: "User not deleted due to internal error" }
+end
+
+
+##################
+
+
+require 'logger'
+
+log = Logger.new(STDERR)
+begin
+  raise
+rescue
+  log.warn('file_name') { "line no." }
+  exit false # Exits with "failure" code
+end
+
+
+
+
+#################
+
 user_name = User.find_by(id: 1).name
 unless user_name
   logger.error "Someone forgot to run db:populate!"
@@ -71,49 +209,6 @@ begin
 rescue => error
   raise unless error.message =~ /foo bar/
 end
-
-
-
-
-
-
-require 'English'
-
-puts $!.inspect
-
-#Output
-=> nil
-
-begin
-  raise "Oops"
-rescue
-  puts $!.inspect
-  puts $ERROR_INFO.inspect
-end
-
-#Output
-
-puts $!.inspect
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
